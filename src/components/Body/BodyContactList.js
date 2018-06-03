0a7c7961524fdaf2426c2e19fb3b0ac9ad8be904
Alexandr <semakaleksandr2014@gmail.com>
2018-06-03 20:46:13 +0300

    added func del & update in BodyContactList.js

--- src/components/Body/BodyContactList.js
+++ src/components/Body/BodyContactList.js
@@ -3,19 +3,34 @@ import Body from './Body'
 import './body.css'
 
 export default class BodyContactList extends Component {
+
+	deleteBlock = (i) => {
+    var arr = this.state.contactElements;
+    arr.splice (i, 1);
+    this.setState ({contactElements: arr});
+  };
+  updateText = (text, i) => {
+    var arr = this.state.contactElements;
+    arr[i] = text;
+    this.setState ({contactElements: arr});
+  };
+
 	render(){
 
 		const contactElements = this.props.boxContact.map ((contact, index) => 
-						<li key = {contact.id} index={index} >
-							<Body contact={contact} /><hr/>
+						//<li key = {contact.id} index={index} >
+						<li key = {index} >
+							<Body contact={contact} update={this.updateText} del={this.deleteBlock} /><hr/>
 						</li>
 					)
 
-		return (
+		return (			
 
 			<ul className="UlBodyContactList">
 
-				{contactElements}		
+				{contactElements}
+
+				{console.log(contactElements)}	
 				
 			</ul>
 
